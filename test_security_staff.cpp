dbc464647e6f46d9a1aa03946c00250c2c59715b
Jack Mcleans <jackagwaya@gmail.com>
2020-01-31 17:07:20 +0300

    database read and inseart TEST_CASES

--- test_security_staff.cpp
+++ test_security_staff.cpp
@@ -1,11 +1,59 @@
 #include "Catch/catch.hpp"
 #include "security_staff.hpp"
 
-TEST_CASE("EVERYTHING IS OK"){
+TEST_CASE("CONNECT TO VEHICLE_ACCESS_CONTROL DATABASE"){
 
-    access_control::Security_staff security;
+    access_control::Security_staff db_staff_details("VEHICLE_ACCESS_CONTROL",
+                                                    "root",
+                                                    "mcleans123");
     
-    REQUIRE(security.present == true);
+    SECTION("CONNECTING TO THE DATABASE"){
+        REQUIRE(db_staff_details.connect() == true);
+    }
+
 }
+
+TEST_CASE("INSERT SECURITY STAFF DATA INTO THE DATABASE"){
+
+    access_control::Security_staff db_staff_details("VEHICLE_ACCESS_CONTROL",
+                                                    "root",
+                                                    "mcleans123");
+    db_staff_details.connect();
+
+    SECTION("INSERT FIRST DATA POINT "){
+
+        Staff_details staff_details = {
+            "JACK MCLEANS",
+            "QME0001",
+            4,
+            "mcleans_jack",
+            "FALSE"
+            };
+
+        REQUIRE(db_staff_details.insert(staff_details ) == true);
+
+        }
+    }
+    
+TEST_CASE("READ SECURITY STAFF DATA FROM DATABASE"){
+
+    access_control::Security_staff db_staff_details("VEHICLE_ACCESS_CONTROL",
+                                                    "root",
+                                                    "mcleans123");
+    db_staff_details.connect();
+
+    Staff_details staff_details;
+
+    staff_details = db_staff_details.read();
+
+    REQUIRE(staff_details.name = "JACK MCLEANS");
+    REQUIRE(staff_details.employment_number = "QME0001");
+    REQUIRE(staff_details.clearence_level = 4);
+    REQUIRE(staff_details.password = "mcleans_jack");
+    REQUIRE(staff_details.is_onduty = "FALSE");
+
+}
+
+
 
 
