55a4bd95fc14e9beeca30053734398af7eaf665e
Jack Mcleans <jackagwaya@gmail.com>
2020-02-03 19:12:34 +0300

    constructor and destructor declaration

--- vehicle_records.cpp
+++ vehicle_records.cpp
@@ -1,5 +1,20 @@
 #include "vehicle_records.hpp"
 
 namespace access_control{
+
+    Vehicle_records::Vehicle_records(std::string db_name,
+                                        std::string db_username,
+                                        std::string db_password):db_name(db_name),
+                                                                    db_username(db_username),
+                                                                    db_password(db_password){
+
+    }
+
+    Vehicle_records::~Vehicle_records(){
+        con = NULL;
+        pstmt = NULL;
+        result = NULL ;
+        stmt = NULL;
+    }
     
 }
