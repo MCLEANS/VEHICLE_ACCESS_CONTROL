e46accef24c642a75c0f2a1d5110625a7a7aaacc
Jack Mcleans <jackagwaya@gmail.com>
2020-02-05 10:37:49 +0300

    definition of delete_record method

--- vehicle_records.cpp
+++ vehicle_records.cpp
@@ -73,14 +73,29 @@ namespace access_control{
     }
 
     bool Vehicle_records::update_vehicle_owner(std::string rfid, std::string new_vehicle_owner){
+        pstmt = con->prepareStatement("UPDATE VEHICLE_RECORDS SET VEHICLE_OWNER = ? WHERE RF_ID = ?");
+        pstmt->setString(1,new_vehicle_owner);
+        pstmt->setString(2,rfid);
+        pstmt->executeQuery();
 
+        return true;
     }
 
     bool Vehicle_records::update_owner_employment_number(std::string rfid, std::string new_employment_number){
 
+        pstmt = con->prepareStatement("UPDATE VEHICLE_RECORDS SET OWNER_EMPLOYMENT_NUMBER = ? WHERE RF_ID = ?");
+        pstmt->setString(1,new_employment_number);
+        pstmt->setString(2,rfid);
+        pstmt->executeQuery();
+
+        return true;
     }
 
     bool Vehicle_records::delete_record(std::string rfid){
-
+        pstmt = con->prepareStatement("DELETE FROM VEHICLE_RECORDS WHERE RF_ID = ?");
+        pstmt->setString(1,rfid);
+        result = pstmt->executeQuery();
+        
+        return true;
     }
 }
