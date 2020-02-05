9a681ec817826063b10a2ce41387f5093a29810a
Jack Mcleans <jackagwaya@gmail.com>
2020-02-05 12:01:32 +0300

    declaration of db_connection class

---
+++ db_connection.hpp
@@ -0,0 +1,27 @@
+#ifndef _DB_CONNECTION_
+#define _DB_CONNECTION_
+
+#include <iostream>
+
+namespace access_control{
+
+    class Vehicle_access_control{
+        private:
+        std::string db_name;
+        std::string db_username;
+        std::string db_password;
+        private:
+        public:
+        public:
+            Vehicle_access_control(std::string db_name,
+                                    std::string db_username,
+                                    std::string db_password);
+
+            bool connect();
+    }
+}
+
+
+
+
+#endif //_DB_CONNECTION_
