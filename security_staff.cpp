d62714b81b387beb20c3ce568bf6614d25c7f792
Jack Mcleans <jackagwaya@gmail.com>
2020-02-05 13:33:36 +0300

    removal of connection method

--- security_staff.cpp
+++ security_staff.cpp
@@ -19,21 +19,6 @@ namespace access_control{
 
     }
 
-  /*  bool Security_staff::connect(){
-        
-
-        try{
-            driver = get_driver_instance();
-            con = driver->connect(this->db_name,this->db_username,this->db_password);
-        }
-        catch(sql::SQLException e){
-            std::cout<<"could not connect to db, ERROR:  "<<e.what()<<std::endl;
-            return false;
-        }
-
-        return true;
-    } */
-
     bool Security_staff::insert(Staff_details &staff_details){
         pstmt = con ->prepareStatement("INSERT INTO SECURITY_STAFF(NAME,EMPLOYMENT_ID,CLEARANCE_LEVEL,PASSWORD,ON_DUTY) VALUES(?,?,?,?,?)");
         pstmt->setString(1,staff_details.name);
