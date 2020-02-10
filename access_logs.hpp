22924a65836a49e6c141cdcf9c199a627ccf96d8
Jack Mcleans <jackagwaya@gmail.com>
2020-02-10 18:44:31 +0300

    declaration of public methods

--- access_logs.hpp
+++ access_logs.hpp
@@ -17,11 +17,29 @@ namespace access_control{
         std::string is_onSite;
     };
 
+    struct Access_details{
+        std::string rfid;
+        std::string time_in;
+        std::string time_out;
+        std::string is_onSite;
+    };
+
     class Access_logs : public Vehicle_access_control{
         private:
+            
         private:
         public:
         public:
+            Access_logs(std::string db_name,
+                        std::string db_username,
+                        std::string db_password);
+
+            bool insert(Access_entrance_details &access_details);
+            bool update(Access_exit_details &access_details);
+            Access_details retreive_by_rfid();
+            
+
+            ~Access_logs();
     };
 
 }
