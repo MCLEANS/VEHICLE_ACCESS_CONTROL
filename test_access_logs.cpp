88cadf78f00420d459d02e3584a86db590a9f533
Jack Mcleans <jackagwaya@gmail.com>
2020-02-10 17:49:05 +0300

    access logs insertion test

--- test_access_logs.cpp
+++ test_access_logs.cpp
@@ -16,3 +16,31 @@ TEST_CASE("CONNECT TO DATABASE"){
         REQUIRE(access_logs.connect() == true);
     }
 }
+
+TEST_CASE("INSERT LOGS INTO THE DATABASE"){
+    access_control::Access_logs access_logs("localhost/VEHICLE_ACCESS_CONTROL",
+                                            "root",
+                                            "mcleans123");
+    access_logs.connect();
+
+    SECTION("FIRST ENTRANCE ACCESS LOG "){
+        access_control::Access_details access_details = {
+            "3E4D52",
+            "A2",
+            "TRUE"
+        };
+
+        REQUIRE(access_logs.insert(access_details) == true);
+
+    }
+    SECTION("SECOND ENTRANCE ACCESS LOG"){
+        access_control::Access_details access_details = {
+            "42E34D2",
+            "A4",
+            "TRUE"
+        };
+
+        REQUIRE(access_logs.insert(access_details) == true);
+    }
+}
+
