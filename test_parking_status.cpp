da49289d25c44708aecbef78c9a662c6d082fb38
Jack Mcleans <jackagwaya@gmail.com>
2020-02-15 21:48:53 +0300

    database connection and table update

--- test_parking_status.cpp
+++ test_parking_status.cpp
@@ -1,2 +1,31 @@
 #include "Catch/catch.hpp"
-#include "parking status.hpp"
+#include "parking_status.hpp"
+
+TEST_CASE("CONNECT TO DATABASE"){
+    access_control::Parking_status parking_status("localhost/VEHICLE_ACCESS_CONTROL",
+                                                    "root",
+                                                    "mcleans123");
+
+    REQUIRE(parking_status.connect() == true);
+
+}
+
+TEST_CASE("UPDATE PARKING STATUS"){
+    access_control::Parking_status parking_status("localhost/VEHICLE_ACCESS_CONTROL",
+                                                    "root",
+                                                    "mcleans123");
+    
+    parking_status.connect();
+
+    SECTION("FIRST PARKING STATUS UPDATE"){
+
+        access_control::Parking_space parking_space = {
+            "A1",
+            "TRUE"
+        };
+
+        parking_status.update(parking_space);
+        CHECK(parking_space.get_status(parking_space.space_id) == "TRUE");
+    }
+
+}
